7f1faa77c0277602fca83d62fb62d47b60175662
AlexWang <yen867@gmail.com>
2018-07-08 01:21:18 +0800

    修正範例路徑

--- newdoc_check_env.bat
+++ newdoc_check_env.bat
@@ -1,9 +1,9 @@
 @echo off
 
-REM == 以下三個檔案請設定在共用資料夾上 ex:\\nas\newdoc\kdapp.jnlp ==
-SET chkHicos=\\nas\p_disk\軟體分享\newdoc\checkHicos.ps1
-SET fileJavaCert=\\nas\p_disk\軟體分享\newdoc\trusted.jssecerts
-SET fileKdapp=\\nas\p_disk\軟體分享\newdoc\kdapp.jnlp
+REM == 以下三個檔案請設定在共用資料夾上 ex:\\nas\share\newdoc\kdapp.jnlp ==
+SET chkHicos=\\nas\share\newdoc\checkHicos.ps1
+SET fileJavaCert=\\nas\share\newdoc\trusted.jssecerts
+SET fileKdapp=\\nas\share\newdoc\kdapp.jnlp
 
 REM ==============================================================
 SET javaExpSites=%userprofile%\AppData\LocalLow\Sun\Java\Deployment\security\exception.sites
